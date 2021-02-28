7d716fee3bbf6c5174862e014b830cf1ee3a2923
Devon Brady <devon.a.brady@gmail.com>
2021-02-27 19:17:10 -0500

    added tables

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,14 +1,28 @@
+  
 exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
       users.increments('user_id')
-      users.string('user_username', 200).notNullable()
-      users.string('user_password', 200).notNullable()
-      users.string('user_email', 320).notNullable()
-      users.timestamps(false, true)
+      users.string('username', 200).unique().notNullable()
+      users.string('password', 200).notNullable()
+      users.integer('phoneNumber', 320).notNullable()
+      // users.timestamps(false, true)
+    })
+    .createTable("plants",tbl => {
+      tbl.increments("plant_id")
+      tbl.string("nickname").notNullable()
+      tbl.string("species").notNullable()
+      tbl.string("h2oFrequency").notNullable()
+      tbl.integer("user_id").notNullable()
+      .unsigned()
+      .references("user_id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE")
     })
 }
 
 exports.down = async (knex) => {
   await knex.schema.dropTableIfExists('users')
+  await knex.schema.dropTableIfExists("plants")
 }
