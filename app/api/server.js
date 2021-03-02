b80018d851ebf421f67e931e1106b960c3716ba3
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2021-03-01 21:16:15 -0500

    fix the undifine error

--- app/api/server.js
+++ app/api/server.js
@@ -5,10 +5,11 @@ const userRouter = require("../api/users/user-router")
 const server = express()
 
 
-server.use("/api/users",userRouter);
+
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+server.use("/api/users",userRouter);
 
 server.get("/",(req,res) => {
     res.json("server up and running")
